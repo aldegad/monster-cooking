d64a86ebdcadcf6030f3b61f32276fea31830833
aldegad <aldegad@naver.com>
2024-03-09 01:03:12 +0900

    fixing movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,19 +7,21 @@ public class PlayerMovement : NetworkBehaviour
 {
     [SerializeField] private Transform spawnPoint;
 
-    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float walkSpeed = 6f;
     [SerializeField] private float runSpeed = 10f;
-    [SerializeField] private float crouchSpeed = 3f;
+    [SerializeField] private float crouchSpeed = 4f;
     [SerializeField] private float rotationSpeed = 10f;
 
     private Rigidbody rigid;
     private Animator anim;
-    private Vector3 inputVector;
+    private Vector3 moveDirection;
+    private float maxSpeed = 0f;
 
     public override void OnNetworkSpawn()
     {
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        maxSpeed = walkSpeed;
 
         if (!IsOwner) { return; }
 
@@ -31,16 +33,16 @@ public class PlayerMovement : NetworkBehaviour
         if (!IsOwner) { return; }
         if (!GameManager.Instance.GetPlayer(OwnerClientId).IsPlayerReady()) { return; }
 
-        UpdateInputVecter();
+        UpdateMoveDirection();
     }
     private void FixedUpdate()
     {
         if (!IsOwner) { return; }
         if (!GameManager.Instance.GetPlayer(OwnerClientId).IsPlayerReady()) { return; }
 
-        if (inputVector == Vector3.zero) { return; }
+        if (moveDirection == Vector3.zero) { return; }
 
-        UpdatePosition(inputVector);
+        UpdatePosition(moveDirection);
     }
 
     private void initializePosition()
@@ -78,20 +80,18 @@ public class PlayerMovement : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     private void updatePositionServerRpc(Vector3 inputVector)
     {
-        Vector3 position = transform.position + inputVector.normalized * walkSpeed * Time.fixedDeltaTime;
+        Vector3 position = transform.position + inputVector.normalized * maxSpeed * Time.fixedDeltaTime;
         rigid.MovePosition(position);
 
         int playerIndex = GameManager.Instance.GetPlayerIndex(OwnerClientId);
         GameManager.Instance.players[playerIndex] = PlayerData.SetPosition(playerIndex, position);
     }
-    private void UpdateInputVecter()
+    private void UpdateMoveDirection()
     {
         // Å° ÀÔ·Â
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector3 inputDirection = new Vector3(horizontal, 0, vertical);
-
-        inputVector = inputDirection.normalized;
+        moveDirection = new Vector3(horizontal, 0, vertical);
 
         // Debug.Log(inputVector);
 
